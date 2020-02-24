25009fb82186c68422dd54b5351f795bb59bc869
Kevin <kevin.floba@gmail.com>
2020-02-24 16:45:26 -0300

    Second Commit

--- app/src/main/java/cl/desafiolatam/desafiologin/MainActivity.java
+++ app/src/main/java/cl/desafiolatam/desafiologin/MainActivity.java
@@ -2,6 +2,7 @@ package cl.desafiolatam.desafiologin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,20 +14,34 @@ import com.squareup.picasso.Target;
 
 public class MainActivity extends AppCompatActivity {
 
-        public ImageView imageView;
-        public Button button;
+        private ImageView imageView;
+        private Button button;
+        private Button buttonRegister;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         imageView = findViewById(R.id.imageView);
         button = findViewById(R.id.button);
+        buttonRegister = findViewById(R.id.buttonRegister);
         Picasso.get().load("http://blog.desafiolatam.com/wp-content/uploads/2019/02/logo.png").into(imageView);
         
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "Usuario ingresado con éxito", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+
             }
         });
 
